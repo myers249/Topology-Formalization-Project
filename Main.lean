3f7ee5a92e720b37f9147faebd785d8764701167
Lean 4 VS Code Extension <>
2026-02-04 11:56:15 -0500

    Initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import TopologyProject
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ TopologyProject.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `TopologyProject` library.
+-- Import modules here that should be built as part of the library.
+import TopologyProject.Basic
